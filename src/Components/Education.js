ffb56d77bb58239372b616d5bfe00f6c8e8e532a
natagr23 <nata_garcia23@hotmail.com>
2021-12-23 16:46:06 -0500

    first commit

---
+++ src/Components/Education.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+//import components
+
+const Education =(props)=>{
+
+return(
+    <div>
+        <section>
+            <h1> Education</h1>
+        </section>
+    </div>
+)
+
+};
+
+export default Education;
---
+++ src/Components/PortafolioItems.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+//import components
+
+const PortafolioItems =(props)=>{
+
+return(
+    <div>
+        <section>
+            <h1> Portafolio Items</h1>
+        </section>
+    </div>
+)
+
+};
+
+export default PortafolioItems;
