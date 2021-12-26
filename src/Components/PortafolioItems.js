c01cc37cf9a69fb7e849a5898220cd59a5abe7e4
natagr23 <nata_garcia23@hotmail.com>
2021-12-26 11:02:30 -0500

    edit styles

--- src/Components/PortafolioItems.js
+++ src/Components/PortafolioItems.js
@@ -1,16 +1,29 @@
+import { render } from '@testing-library/react';
 import React, { useState } from 'react';
 
 //import components
 
-const PortafolioItems =(props)=>{
+const PortafolioItems = (props) => {
+    //JavaScript code
+    const onLinkClick = (c)=>{
+        c.preventDefault();
+      console.log('this link was clicked');
+      window.location.href='https://wordpress.garcia-dev.es/';
+    };
 
-return(
-    <div>
-        <section>
-            <h1> Portafolio Items</h1>
-        </section>
-    </div>
-)
+    
+
+    return (
+        <div>
+          
+            <button><a href='https://wordpress.garcia-dev.es/' onClick={onLinkClick}>{props.me1}</a></button>
+            
+           
+                <button>{props.me2}</button>
+                <button>{props.me3}</button>
+           
+        </div>
+    )
 
 };
 
